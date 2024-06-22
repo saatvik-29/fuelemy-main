0bf20e8666e038a23616e1efa0efa6594e3beafb
saatvik-29 <121422100+saatvik-29@users.noreply.github.com>
2024-06-22 11:52:38 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ const app = express();
 connectDB();
 
 app.use(cors());
+app.use(cors({
+  origin: ["https://deploy-mern-1whq.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 app.use(express.json({ extended: false }));
 
